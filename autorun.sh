313b23dd33cfeeacce6d15d57a7e507410f64c74
Tomás Afonso <168651587+thomraider12@users.noreply.github.com>
2025-12-11 18:57:53 +0000

    Replace pip install packages in autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -2,8 +2,7 @@
 
 echo $(dirname $0)
 
-python3 -m pip install requests beautifulsoup4 tqdm
-
+python3 -m pip install yt-dlp
 cd $(dirname $0)/scripts/
 
 python3 youtube_m3ugrabber.py -i ../youtube_channel_info.txt -o canais.m3u
